b9223b135b15f9bcc6efd997bbc204e34d24ebdd
Sixze <16257871+Sixze@users.noreply.github.com>
2022-08-24 18:45:13 +0300

    Add UFuBTDecorator_CanActivateAbility behavior tree decorator

---
+++ Source/FabulousUtility/Public/AI/FuBTDecorator_CanActivateAbility.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "BehaviorTree/BTDecorator.h"
+#include "FuBTDecorator_CanActivateAbility.generated.h"
+
+class UFuAbilitySystemComponent;
+struct FGameplayAbilitySpec;
+
+UCLASS(DisplayName = "Fu Can Activate Ability", Meta = (ShowWorldContextPin))
+class FABULOUSUTILITY_API UFuBTDecorator_CanActivateAbility : public UBTDecorator
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Settings")
+	FGameplayTagContainer AbilityTags;
+
+public:
+	UFuBTDecorator_CanActivateAbility();
+
+	virtual uint16 GetInstanceMemorySize() const override;
+
+	virtual FString GetStaticDescription() const override;
+
+protected:
+	virtual void OnBecomeRelevant(UBehaviorTreeComponent& BehaviorTree, uint8* NodeMemory) override;
+
+	virtual void OnCeaseRelevant(UBehaviorTreeComponent& BehaviorTree, uint8* NodeMemory) override;
+
+	virtual void TickNode(UBehaviorTreeComponent& BehaviorTree, uint8* NodeMemory, float DeltaTime) override;
+
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& BehaviorTree, uint8* NodeMemory) const override;
+
+private:
+	void OnAbilityGiven(UFuAbilitySystemComponent* AbilitySystem, const FGameplayAbilitySpec& AbilitySpecification,
+	                    TWeakObjectPtr<UBehaviorTreeComponent> BehaviorTree);
+
+	void OnAbilityRemoved(UFuAbilitySystemComponent* AbilitySystem, const FGameplayAbilitySpec& AbilitySpecification,
+	                      TWeakObjectPtr<UBehaviorTreeComponent> BehaviorTree);
+};
