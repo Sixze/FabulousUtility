15aedeb6981464fb388dbed4d81396984b8a0731
Sixze <16257871+Sixze@users.noreply.github.com>
2022-10-17 19:56:35 +0300

    Add UFuAbilityTask_EffectListener ability task

---
+++ Source/FabulousUtility/Private/AbilitySystem/AbilityTasks/FuAbilityTask_EffectListener.cpp
@@ -0,0 +1,94 @@
+#include "AbilitySystem/AbilityTasks/FuAbilityTask_EffectListener.h"
+
+#include "FuMacros.h"
+#include "AbilitySystem/FuAbilitySystemComponent.h"
+
+UFuAbilityTask_EffectListener* UFuAbilityTask_EffectListener::FuWaitForEffectByTag(
+	UGameplayAbility* OwningAbility, const FGameplayTag EffectTag)
+{
+	auto* Task{NewAbilityTask<ThisClass>(OwningAbility)};
+
+	if (FU_ENSURE(EffectTag.IsValid()))
+	{
+		Task->EffectTags1.AddTag(EffectTag);
+	}
+
+	return Task;
+}
+
+UFuAbilityTask_EffectListener* UFuAbilityTask_EffectListener::FuWaitForEffectByTags(
+	UGameplayAbility* OwningAbility, const FGameplayTagContainer EffectTags)
+{
+	auto* Task{NewAbilityTask<ThisClass>(OwningAbility)};
+
+	for (const auto& Tag : EffectTags)
+	{
+		if (FU_ENSURE(Tag.IsValid()))
+		{
+			Task->EffectTags1.AddTag(Tag);
+		}
+	}
+
+	return Task;
+}
+
+void UFuAbilityTask_EffectListener::Activate()
+{
+	Super::Activate();
+
+	auto* AbilitySystem{Cast<UFuAbilitySystemComponent>(AbilitySystemComponent)};
+
+	if (!FU_ENSURE(IsValid(AbilitySystem)) || EffectTags1.IsEmpty())
+	{
+		EndTask();
+		return;
+	}
+
+	AbilitySystem->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(this, &ThisClass::OnActiveGameplayEffectAdded);
+	AbilitySystem->OnAnyGameplayEffectRemovedDelegate().AddUObject(this, &ThisClass::OnActiveGameplayEffectRemoved);
+
+	if (!ShouldBroadcastAbilityTaskDelegates())
+	{
+		return;
+	}
+
+	FScopedActiveGameplayEffectLock EffectScopeLock{AbilitySystem->GetActiveEffects()};
+
+	for (const auto& ActiveEffect : &AbilitySystem->GetActiveEffects())
+	{
+		if (ActiveEffect.Spec.CapturedSourceTags.GetSpecTags().HasAny(EffectTags1))
+		{
+			OnEffectAdded.Broadcast(ActiveEffect.Handle);
+		}
+	}
+}
+
+void UFuAbilityTask_EffectListener::OnDestroy(const bool bInOwnerFinished)
+{
+	auto* AbilitySystem{Cast<UFuAbilitySystemComponent>(AbilitySystemComponent)};
+	if (IsValid(AbilitySystem))
+	{
+		AbilitySystem->OnActiveGameplayEffectAddedDelegateToSelf.RemoveAll(this);
+		AbilitySystem->OnAnyGameplayEffectRemovedDelegate().RemoveAll(this);
+	}
+
+	Super::OnDestroy(bInOwnerFinished);
+}
+
+void UFuAbilityTask_EffectListener::OnActiveGameplayEffectAdded(UAbilitySystemComponent* AbilitySystem,
+                                                                const FGameplayEffectSpec& EffectSpecification,
+                                                                const FActiveGameplayEffectHandle EffectHandle) const
+{
+	if (ShouldBroadcastAbilityTaskDelegates() && EffectSpecification.CapturedSourceTags.GetSpecTags().HasAny(EffectTags1))
+	{
+		OnEffectAdded.Broadcast(EffectHandle);
+	}
+}
+
+void UFuAbilityTask_EffectListener::OnActiveGameplayEffectRemoved(const FActiveGameplayEffect& ActiveEffect) const
+{
+	if (ShouldBroadcastAbilityTaskDelegates() && ActiveEffect.Spec.CapturedSourceTags.GetSpecTags().HasAny(EffectTags1))
+	{
+		OnEffectRemoved.Broadcast(ActiveEffect.Handle);
+	}
+}
---
+++ Source/FabulousUtility/Public/AbilitySystem/AbilityTasks/FuAbilityTask_EffectListener.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Abilities/Tasks/AbilityTask.h"
+#include "FuAbilityTask_EffectListener.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FFuEffectChangedDelegate, FActiveGameplayEffectHandle, EffectHandle);
+
+UCLASS(DisplayName = "Fu Effect Listener Ability Task")
+class FABULOUSUTILITY_API UFuAbilityTask_EffectListener : public UAbilityTask
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "State", Transient, DisplayName = "Effect Tags", Meta = (AllowPrivateAccess))
+	FGameplayTagContainer EffectTags1;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "Fabulous Utility|Fu Effect Listener Ability Task")
+	FFuEffectChangedDelegate OnEffectAdded;
+
+	UPROPERTY(BlueprintAssignable, Category = "Fabulous Utility|Fu Effect Listener Ability Task")
+	FFuEffectChangedDelegate OnEffectRemoved;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Fabulous Utility|Fu Ability", BlueprintInternalUseOnly,
+		Meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility"))
+	static UFuAbilityTask_EffectListener* FuWaitForEffectByTag(UGameplayAbility* OwningAbility, FGameplayTag EffectTag);
+
+	UFUNCTION(BlueprintCallable, Category = "Fabulous Utility|Fu Ability", BlueprintInternalUseOnly,
+		Meta = (HidePin = "OwningAbility", DefaultToSelf = "OwningAbility"))
+	static UFuAbilityTask_EffectListener* FuWaitForEffectByTags(UGameplayAbility* OwningAbility, FGameplayTagContainer EffectTags);
+
+protected:
+	virtual void Activate() override;
+
+	virtual void OnDestroy(bool bInOwnerFinished) override;
+
+private:
+	void OnActiveGameplayEffectAdded(UAbilitySystemComponent* AbilitySystem,
+	                                 const FGameplayEffectSpec& EffectSpecification,
+	                                 FActiveGameplayEffectHandle EffectHandle) const;
+
+	void OnActiveGameplayEffectRemoved(const FActiveGameplayEffect& ActiveEffect) const;
+};
