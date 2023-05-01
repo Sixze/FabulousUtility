6315f73b756c59da76e78ea1fcb9d33a78a1bb6c
Sixze <16257871+Sixze@users.noreply.github.com>
2023-05-01 15:42:15 +0300

    Update UFuCommonUIUtility

--- Source/FabulousUtility/Private/UI/FuCommonUIUtility.cpp
+++ Source/FabulousUtility/Private/UI/FuCommonUIUtility.cpp
@@ -8,11 +8,17 @@ ECommonInputType UFuCommonUIUtility::GetInputTypeForKey(const FKey& Key)
 {
 	// Based on FCommonInputPreprocessor::GetInputType().
 
-	return !Key.IsGamepadKey()
-		       ? ECommonInputType::MouseAndKeyboard
-		       : !UCommonInputSubsystem::IsMobileGamepadKey(Key)
-		       ? ECommonInputType::Gamepad
-		       : ECommonInputType::Touch;
+	if (!Key.IsGamepadKey())
+	{
+		return ECommonInputType::MouseAndKeyboard;
+	}
+
+	if (!UCommonInputSubsystem::IsMobileGamepadKey(Key))
+	{
+		return ECommonInputType::Gamepad;
+	}
+
+	return ECommonInputType::Touch;
 }
 
 UUserWidget* UFuCommonUIUtility::FindRootUserWidgetOfClass(const UWidget* Widget, TSubclassOf<UUserWidget> UserWidgetClass)
@@ -44,4 +50,33 @@ UUserWidget* UFuCommonUIUtility::FindRootUserWidgetOfClass(const UWidget* Widget
 	}
 
 	return ResultUserWidget;
+}
+
+UUserWidget* UFuCommonUIUtility::FindParentUserWidgetOfClass(const UWidget* Widget, TSubclassOf<UUserWidget> UserWidgetClass)
+{
+	if (!IsValid(UserWidgetClass))
+	{
+		UserWidgetClass = UUserWidget::StaticClass();
+	}
+
+	while (IsValid(Widget))
+	{
+		// The outer of every widget is the UWidgetTree it's in, and the outer of every UWidgetTree is the UUserWidget that owns it.
+
+		const UWidgetTree* WidgetTree{Cast<UWidgetTree>(Widget->GetOuter())};
+		if (!IsValid(WidgetTree))
+		{
+			return nullptr;
+		}
+
+		auto* UserWidget{Cast<UUserWidget>(WidgetTree->GetOuter())};
+		if (IsValid(UserWidget) && UserWidget->IsA(UserWidgetClass))
+		{
+			return UserWidget;
+		}
+
+		Widget = UserWidget;
+	}
+
+	return nullptr;
 }
