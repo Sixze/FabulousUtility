ff8231e61363687c34954d564d7c7883bfee2479
Sixze <16257871+Sixze@users.noreply.github.com>
2023-06-12 18:34:42 +0300

    Add the FabulousUI module

---
+++ Source/FabulousUI/FabulousUI.Build.cs
@@ -0,0 +1,14 @@
+using UnrealBuildTool;
+
+public class FabulousUI : ModuleRules
+{
+	public FabulousUI(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
+
+		bEnableNonInlinedGenCppWarnings = true;
+
+		PrivateDependencyModuleNames.AddRange(new[] {"Core", "CoreUObject", "Engine"});
+	}
+}
