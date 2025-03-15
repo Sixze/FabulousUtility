98275b4a0ec47d04cb5dff6fcbf513caa5074f11
Sixze <16257871+Sixze@users.noreply.github.com>
2025-03-15 15:14:46 +0300

    Add UFuIntersectionUtility

---
+++ Source/FabulousUtility/Private/Math/FuIntersectionUtility.cpp
@@ -0,0 +1,36 @@
+﻿#include "Math/FuIntersectionUtility.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FuIntersectionUtility)
+
+bool UFuIntersectionUtility::LineSegmentIntersection2D(const FVector2D& LineOrigin, const FVector2D& LineDirection,
+                                                       const FVector2D& SegmentStart, const FVector2D& SegmentEnd,
+                                                       FVector2D& Intersection)
+{
+	// https://stackoverflow.com/questions/2931573/determining-if-two-rays-intersect
+
+	const auto SegmentVector{SegmentEnd - SegmentStart};
+	const FVector2D SegmentVectorPerpendicular{SegmentVector.Y, -SegmentVector.X};
+
+	const auto Dot{LineDirection | SegmentVectorPerpendicular};
+	if (FMath::IsNearlyZero(Dot))
+	{
+		// The line and the segment are nearly parallel.
+		Intersection = FVector2D::ZeroVector;
+		return false;
+	}
+
+	const auto DotInverse{1.0f / Dot};
+	const auto SegmentStartRelative{SegmentStart - LineOrigin};
+
+	// const auto LineTime{(SegmentStartRelative ^ SegmentVector) * DotInverse};
+	const auto SegmentTime{(SegmentStartRelative ^ LineDirection) * DotInverse};
+
+	if (SegmentTime < 0.0f || SegmentTime > 1.0f)
+	{
+		Intersection = FVector2D::ZeroVector;
+		return false;
+	}
+
+	Intersection = SegmentStart + SegmentVector * SegmentTime;
+	return true;
+}
---
+++ Source/FabulousUtility/Public/Math/FuIntersectionUtility.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FuIntersectionUtility.generated.h"
+
+UCLASS(Meta = (BlueprintThreadSafe))
+class FABULOUSUTILITY_API UFuIntersectionUtility : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Fabulous Utility|Intersection Utility",
+		Meta = (AutoCreateRefTerm = "LineOrigin, LineDirection, SegmentStart, SegmentEnd", ReturnDisplayName = "Success"))
+	static bool LineSegmentIntersection2D(const FVector2D& LineOrigin, const FVector2D& LineDirection,
+	                                      const FVector2D& SegmentStart, const FVector2D& SegmentEnd,
+	                                      FVector2D& Intersection);
+};
