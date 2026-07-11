426fda0f8ac9ec4cdcda01602f887ac373b4a708
Sixze <16257871+Sixze@users.noreply.github.com>
2026-07-11 15:35:52 +0300

    Add UFuMath::SmoothMax()

--- Source/FabulousUtility/Public/Math/FuMath.h
+++ Source/FabulousUtility/Public/Math/FuMath.h
@@ -9,6 +9,9 @@ class FABULOUSUTILITY_API UFuMath : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
+	template <typename ValueType> requires UE::CFloatingPoint<ValueType>
+	static constexpr ValueType SmoothMax(ValueType A, ValueType B, float Lambda);
+
 	UFUNCTION(BlueprintPure, Category = "Fabulous Utility|Math Utility", Meta = (ReturnDisplayName = "Value"))
 	static float Clamp01(float Value);
 
@@ -19,6 +22,14 @@ public:
 	template <typename ValueType>
 	static ValueType DamperExact(const ValueType& Current, const ValueType& Target, float DeltaTime, float HalfLife);
 };
+
+template <typename ValueType> requires UE::CFloatingPoint<ValueType>
+constexpr ValueType UFuMath::SmoothMax(const ValueType A, const ValueType B, const float Lambda)
+{
+	// https://www.youtube.com/watch?v=6Qb6QtC6QMs
+
+	return (A + B + FMath::Sqrt(FMath::Square(A - B) + FMath::Max(0.0f, Lambda))) / 2.0f;
+}
 
 inline float UFuMath::Clamp01(const float Value)
 {
